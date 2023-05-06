ccc61afa74aec46e21e7fff4e36dc28fbbab9ca0
Stan U <89152624+StanFromIreland@users.noreply.github.com>
2023-05-06 16:01:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-#TBD
 const cryptocurrency = 'brick';
-const currency = 'eth';
-
+const currency = 'usd';
 
 const url = `https://api.coingecko.com/api/v3/simple/price?ids=${cryptocurrency}&vs_currencies=${currency}`;
 
-
 axios.get(url)
   .then(response => {
-
     const price = response.data[cryptocurrency][currency];
-    
     document.getElementById('price').innerText = `$${price.toFixed(2)}`;
   })
   .catch(error => {
