09a11169d019aa4bd9906ee704a1c8969aecd4d9
Stan U <89152624+StanFromIreland@users.noreply.github.com>
2023-05-06 15:50:39 +0100

    Update script.js
    brick price

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 #TBD
+const cryptocurrency = 'brick';
+const currency = 'eth';
+
+
+const url = `https://api.coingecko.com/api/v3/simple/price?ids=${cryptocurrency}&vs_currencies=${currency}`;
+
+
+axios.get(url)
+  .then(response => {
+
+    const price = response.data[cryptocurrency][currency];
+    
+    document.getElementById('price').innerText = `$${price.toFixed(2)}`;
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+
+
+
+
