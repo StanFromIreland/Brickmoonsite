eef44d87fa0c12f6c3871474d08e03b0334f9a1d
Stan U <89152624+StanFromIreland@users.noreply.github.com>
2023-05-07 08:27:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,76 +3,42 @@ document.addEventListener("DOMContentLoaded", function() {
   const chartContainer = document.querySelector('#chart');
 
   function displayPrice() {
-    fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${coinId}&vs_currencies=usd`)
+    fetch(`https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=usd&days=7&interval=daily`)
       .then(response => response.json())
       .then(data => {
-        const price = data[coinId].usd;
+        const price = data.prices[data.prices.length - 1][1];
         const priceElement = document.querySelector('.price');
         priceElement.textContent = '$' + price.toFixed(8);
+        displayChart(data); // Call the displayChart() function with the fetched data
+      });
+  }
 
-        // Chart
-        axios.get(`https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=usd&days=7`)
-          .then(response => {
-            const chartData = response.data.prices.map(price => {
-              return {
-                x: price[0],
-                y: price[1]
-              }
-            });
-            const chartOptions = {
-              plugins: {
-                title: {
-                  display: true,
-                  text: `${coinId.toUpperCase()} Price Chart`
-                },
-                legend: {
-                  display: false
-                }
-              },
-              scales: {
-                xAxes: [{
-                  type: 'time',
-                  time: {
-                    unit: 'day',
-                    tooltipFormat: 'MMM DD, YYYY'
-                  },
-                  ticks: {
-                    source: 'auto'
-                  },
-                  gridLines: {
-                    display: false
-                  }
-                }],
-                yAxes: [{
-                  ticks: {
-                    beginAtZero: false
-                  },
-                  gridLines: {
-                    color: '#ddd'
-                  }
-                }]
-              }
-            };
-            const chart = new Chart(chartContainer, {
-              type: 'line',
-              data: {
-                datasets: [{
-                  data: chartData,
-                  borderColor: '#3e95cd',
-                  fill: false
-                }]
-              },
-              options: chartOptions
-            });
-          })
-          .catch(error => console.error(error));
-      })
-      .catch(error => console.error(error));
+  function displayChart(data) {
+    const prices = data.prices.map(price => price[1]);
+    const timestamps = data.prices.map(price => price[0]);
+
+    const options = {
+      chart: {
+        type: 'line'
+      },
+      series: [
+        {
+          name: 'BRICK Price',
+          data: prices
+        }
+      ],
+      xaxis: {
+        type: 'datetime',
+        categories: timestamps
+      }
+    };
+
+    const chart = new ApexCharts(chartContainer, options); // Render the chart to the correct container
+    chart.render();
   }
 
   setInterval(displayPrice, 30000);
 });
-
 
 
 
